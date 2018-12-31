9baaa86335f01fc448f6f4bc7a496849556321d0
Kareem McDavid <kareemmcdavid@hotmail.com>
2018-12-31 04:28:23 -0500

    Added cooldown to kyle command

--- commands/kyle.js
+++ commands/kyle.js
@@ -4,6 +4,7 @@ const pool = db.pool;
 module.exports = {
   name: 'kyle',
   description: 'Counts the number of times I want to ban Kyle',
+  cooldown: 30,
   execute(message, args) {
     const upd = ('UPDATE kyle SET count = count + 1 WHERE key = 0');
     const query = ('SELECT count FROM kyle WHERE key = 0');
