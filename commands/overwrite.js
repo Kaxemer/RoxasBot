23aff535ad043449adb7268552dac3f602dd7e47
Kareem McDavid <kareemmcdavid@hotmail.com>
2019-01-16 22:52:02 -0500

    Mini update

--- commands/overwrite.js
+++ commands/overwrite.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'overwrite',
   description: 'Changes channel perms',
   execute(message, args) {
+    if (!message.author.hasPermission('MANAGE_CHANNELS')) return message.reply('Not possible.'); 
     var chans = message.guild.channels;
     chans.forEach(function(value, key, map) {
       value.overwritePermissions('535281988320362520', {
